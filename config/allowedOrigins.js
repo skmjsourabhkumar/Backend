b989be0f7a9a1f100e81b0552375ce09e0f9a8c6
SOURABH KUMAR <119873930+skmjsourabhkumar@users.noreply.github.com>
2024-07-24 22:38:01 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://sweet-fairy-436336.netlify.app/'
+    'https://sweet-fairy-436336.netlify.app'
 
 ];
 
